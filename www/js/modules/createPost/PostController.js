dc902f193c7dceb26a285c425c1ad042501d797f
Mohandm <atbmohan@gmail.com>
2015-10-20 08:40:20 +0530

    post caard

--- www/js/modules/createPost/PostController.js
+++ www/js/modules/createPost/PostController.js
@@ -1,19 +1,20 @@
 (function () {
-    'use strict';
-    angular.module('app.post').controller('PostController', PostController);
-    /*ng-inject*/
-    function PostController($firebaseArray, Auth, User) {
+	'use strict';
+	angular.module('app.post').controller('PostController', PostController);
+	/*ng-inject*/
+	function PostController($firebaseArray, Auth, User) {
 
-        var vm = this;
-        var itemsRef = new Firebase("https://ionicfiremohan.firebaseio.com/items");
-        var ref = $firebaseArray(itemsRef);
-        vm.post = {};
-        vm.submit = function () {
-            var object = vm.post;
-            object.user = User.getUser();
-            ref.$add({
-                object
-            });
-        }
-    }
+		var vm = this;
+		var itemsRef = new Firebase("https://ionicfiremohan.firebaseio.com/items");
+		vm.postArray = $firebaseArray(itemsRef);
+		console.log(vm.postArray);
+		vm.post = {};
+		vm.submit = function () {
+			var object = vm.post;
+			object.user = User.getUser();
+			vm.postArray.$add({
+				object
+			});
+		}
+	}
 })();
